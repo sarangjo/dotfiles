21ef771452b4a4baf1a0fbe7d06d0db283411678
Sarang Joshi <sarang.joshi@illumio.com>
2018-11-16 14:19:59 -0800

    Add directory detection

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cp bash/.* ~ &> /dev/null
-cp vim/.* ~ &> /dev/null
-cp tmux/.* ~ &> /dev/null
-cp git/.* ~ &> /dev/null
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cp "$DIR"/bash/.* ~ &> /dev/null
+cp "$DIR"/vim/.* ~ &> /dev/null
+cp "$DIR"/tmux/.* ~ &> /dev/null
+cp "$DIR"/git/.* ~ &> /dev/null
 
 exit 0
